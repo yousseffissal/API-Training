7de4cebd939c121cd8c84801d03fa767eba8e689
Youssef Fissal <youssefpaxar1@gmail.com>
2026-02-11 14:32:42 +0100

    Added an error element & AllMessages element

--- frontend/src/routers/router.jsx
+++ frontend/src/routers/router.jsx
@@ -3,12 +3,15 @@ import { createBrowserRouter, } from "react-router-dom";
 // Components to use/manipulate for each route
 import App from "../App.jsx";
 import MainPage from "../Pages/MainPage.jsx";
-
+import AllMessages from "../Pages/AllMessages.jsx";
+import ErrorPage from "../Pages/ErrorPage.jsx";
 const router = createBrowserRouter([
     {
         // In the root we will mount the App component that has the outlet component
         path: "/",
         element: <App />,
+        // The error element will mount when we have an error (The error type proccessing is in the element's logic)
+        errorElement: <ErrorPage />,
         children: [
             {
                 // In the root the outlet component shows the Main Page
@@ -16,14 +19,19 @@ const router = createBrowserRouter([
                 element: <MainPage />,
             },
             {
+                // In the All Route it will show all the messages in the database (Just for testing)
+                path: "/All",
+                element: <AllMessages />,
+            },
+            {
                 // In the Add Route it will show this message (Just for testing)
                 path: "/Add",
-                element: <div>Test for the route Add</div>,
+                element: <div>This is a test for the route Add</div>,
             },
             {
                 // In the Test Route it will show this message (Just for testing)
                 path: "/Test",
-                element: <div>Test for the route Test</div>,
+                element: <div>This is a test for the route Test</div>,
             },
         ]
     },
