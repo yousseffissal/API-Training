221a5b0bd6bc2fff5fba998d7553260bfde7f77b
Youssef Fissal <youssefpaxar1@gmail.com>
2026-02-11 09:24:14 +0100

    Added a router provider (Removed the strict mode) with our router

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,12 @@
+// Strict ode is used if we have one page/static page but here we want to add many pages with many routes so we need to install react-router-dom.
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+// We will use the RouterProvider to manipulate our routes
+import { RouterProvider } from 'react-router-dom'
+// This is the router that we made that will help us to manipulate the routes and which component will mount on the screen when we go to this route
+import router from './routers/router.jsx'
 import './index.css'
-import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <RouterProvider router={router} />
 )
